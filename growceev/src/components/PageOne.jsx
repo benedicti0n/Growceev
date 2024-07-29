3e3d5a981679cb695690c1c18d70ec342b46ee3c
Subhamay-Dey <ncdey1966@gmail.com>
2024-07-30 01:57:20 +0530

    Updated - animated button completed used framer motion.

--- growceev/src/components/PageOne.jsx
+++ growceev/src/components/PageOne.jsx
@@ -7,6 +7,7 @@ import { HiArrowLongDown } from "react-icons/hi2";
 
 
 function PageOne() {
+
   return (
     <div className="min-h-screen w-full flex flex-col justify-center py-10 items-center md:px-12 px-5">
         <div className="w-full flex justify-between items-center">
@@ -16,8 +17,8 @@ function PageOne() {
                 <p className='text-sm md:text-[20px] md:leading-[25.2px] text-gray-800 font-[400] text-left'>Some other sub heading, that you want to add</p>
                 {/* <button className=" w-fit mt-20 rounded-[32px] px-6 py-3 gap-1 flex justify-center items-center bg-black text-white">Book Now <HiArrowLongRight size={20} className='text-white'/>
                 </button> */}
-                <div className=" w-fit mt-20 rounded-[32px] px-6 py-3 gap-1 flex justify-center items-center bg-black text-white">
-                  <Button/>
+                <div className=" w-fit mt-20 px-6 py-3 gap-1 flex justify-center items-cente cursor-pointer">
+                  <Button name={"Book Now"} color={"#0a00c1"}/>
                 </div>
                 
             </div>
