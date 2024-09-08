f47291f0f558793d5287b514fb3e560a4ea66f92
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-09-09 00:26:48 +0530

    minor ui changes

--- growceev/src/components/WhyUs.jsx
+++ growceev/src/components/WhyUs.jsx
@@ -8,7 +8,7 @@ function WhyUs() {
       id: 1,
       content: (
         <div className="flex justify-center items-center gap-3">
-          <FaCheckCircle className="text-green-500" size={20} />
+          <FaCheckCircle className="text-green-500" size={25} />
           <div className="block space-y-2">
             <p>Comprehensive strategy</p>
             <p>development and in-depth pre-</p>
@@ -21,7 +21,7 @@ function WhyUs() {
       id: 2,
       content: (
         <div className="flex gap-3 justify-center items-center">
-          <FaCheckCircle className="text-green-500" size={20} />
+          <FaCheckCircle className="text-green-500" size={25} />
           <div className="block space-y-2">
             <p>Highly skilled,</p>
             <p>certified team with a proven</p>
@@ -34,7 +34,7 @@ function WhyUs() {
       id: 3,
       content: (
         <div className="flex gap-3 justify-center items-center">
-          <FaCheckCircle className="text-green-500" size={20} />
+          <FaCheckCircle className="text-green-500" size={25} />
           <div className="block space-y-2">
             <p>Transparent pricing</p>
             <p> and full data ownership</p>
@@ -46,7 +46,7 @@ function WhyUs() {
       id: 4,
       content: (
         <div className="flex gap-3 justify-center items-center">
-          <FaCheckCircle className="text-green-500" size={20} />
+          <FaCheckCircle className="text-green-500" size={25} />
           <p>Measurable and guaranteed results</p>
         </div>
       ),
@@ -58,7 +58,7 @@ function WhyUs() {
       id: 1,
       content: (
         <div className="flex gap-3 justify-center items-center">
-          <FaCircleXmark className="text-red-500" size={20} />
+          <FaCircleXmark className="text-red-500" size={25} />
           <p>Outsourcing of work.</p>
         </div>
       ),
@@ -67,7 +67,7 @@ function WhyUs() {
       id: 2,
       content: (
         <div className="flex gap-3 justify-center items-center">
-          <FaCircleXmark className="text-red-500" size={20} />
+          <FaCircleXmark className="text-red-500" size={25} />
           <p>Untrained and Intern Level Team</p>
         </div>
       ),
@@ -76,7 +76,7 @@ function WhyUs() {
       id: 3,
       content: (
         <div className="flex gap-3 justify-center items-center">
-          <FaCircleXmark className="text-red-500" size={20} />
+          <FaCircleXmark className="text-red-500" size={25} />
           <div className="block space-y-2">
             <p>Hidden fees and holding data</p>
             <p>hostage on their own accounts</p>
@@ -88,7 +88,7 @@ function WhyUs() {
       id: 4,
       content: (
         <div className="flex gap-3 justify-center items-center">
-          <FaCircleXmark className="text-red-500" size={20} />
+          <FaCircleXmark className="text-red-500" size={25} />
           <div className="block space-y-2">
             <p>No performance milestone model</p>
             <p>when it comes to SEO</p>
@@ -100,11 +100,11 @@ function WhyUs() {
 
   return (
     <div className="min-h-screen w-full flex flex-col justify-center items-center py-10">
-      <h1 className="text-center text-7xl tracking-wide font-bold mt-20 pt-10">Why Us?</h1>
+      <h1 className="text-center text-7xl tracking-wide font-semibold mt-20 pt-10">Why Us?</h1>
       <div className="w-[800px] h-[600px] flex justify-between items-center mt-20">
         {/* Table 1 */}
         <div className="w-[45%] h-full flex flex-col space-y-8 items-start">
-        <h2 className='font-bold flex justify-center items-center text-lg'>Growceev</h2>
+        <h2 className='font-bold flex justify-center items-center text-lg ml-10'>Growceev</h2>
           {table1.map((t1) => (
             <div key={t1.id}>
               {t1.content}
@@ -114,7 +114,7 @@ function WhyUs() {
 
         {/* Table 2 */}
         <div className="w-[45%] h-full flex flex-col space-y-8 items-start">
-        <h2 className='font-bold text-lg flex justify-center items-center'>Other Agencies</h2>
+        <h2 className='font-bold text-lg flex justify-center items-center ml-10'>Other Agencies</h2>
           {table2.map((t2) => (
             <div key={t2.id}>
               {t2.content}
